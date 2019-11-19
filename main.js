8cb6137d57b36e9069672f6902b9ee774a95fc8f
vkorytska <36221452+vkorytska@users.noreply.github.com>
2019-11-19 16:46:22 +0200

    11

--- main.js
+++ main.js
@@ -1567,9 +1567,9 @@ var LogInComponent = /** @class */ (function () {
     };
     LogInComponent.prototype.changeLocation = function () {
         localStorage.setItem('isActivated', 'true');
-        this.router.navigateByUrl('/myProfile');
         var elem = document.body;
         this.setStyleAttribute(elem, { 'overflow': 'auto' });
+        this.router.navigateByUrl('/myProfile');
     };
     tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"])(),
