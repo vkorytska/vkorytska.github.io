085fa7488825fa89e322e4145655b1dac47ddd91
vkorytska <36221452+vkorytska@users.noreply.github.com>
2019-11-21 11:40:06 +0200

    8

--- main.js
+++ main.js
@@ -274,7 +274,7 @@ var UserDialogComponent = /** @class */ (function () {
     };
     UserDialogComponent.prototype.logOut = function () {
         localStorage.setItem('isActivated', 'false');
-        window.location.href = '/main';
+        window.location.href = '/#/main';
         window.location.reload(true);
     };
     UserDialogComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
@@ -2564,7 +2564,7 @@ var HeaderComponent = /** @class */ (function () {
     HeaderComponent.prototype.LogOut = function () {
         localStorage.setItem('isActivated', 'false');
         this.isActivated = false;
-        window.location.href = '/main';
+        window.location.href = '/#/main';
         window.location.reload(true);
     };
     HeaderComponent.prototype.setStyleAttribute = function (element, attrs) {
